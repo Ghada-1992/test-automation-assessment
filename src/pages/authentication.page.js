c28287be40afc19f296980b4d9c26bb6c2c515e4
Ghada Ayari <gayari@iobeya.com>
2022-05-09 01:44:27 +0100

    add authentication page

---
+++ src/pages/authentication.page.js
@@ -0,0 +1,35 @@
+const BasePage = require('./base.page');
+
+class AuthenticationPage extends BasePage {
+    constructor(page) {
+        super(page);
+
+        this.createEmailField = '#email_create';
+        this.submitCreateButton = '#SubmitCreate';
+
+        this.registeredEmailField = '#email';
+        this.registeredPasswordField = '#passwd';
+        this.signInButton = 'SubmitLogin';
+    }
+
+    async enterCreateEmail(createEmail) {
+        await this.page.fill(this.createEmailField, createEmail);
+    }
+
+    async submitCreateEmail() {
+        await this.page.click(this.submitCreateButton);
+    }
+
+    async enterRegisteredEmail(registeredEmail) {
+        await this.page.fill(this.registeredEmailField, registeredEmail);
+    }
+
+    async enterRegisteredPassword(registeredPassword) {
+        await this.page.fill(this.registeredPasswordField, registeredPassword);
+    }
+
+    async clickSignIn() {
+        await this.page.click(this.signInButton);
+    }
+}
+module.exports = AuthenticationPage;
