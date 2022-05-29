e7b9dfc17528df308d9aa60d3b1d1d2b297e35f6
Ghada Ayari <gayari@iobeya.com>
2022-05-29 19:27:56 +0100

    update create account tests

--- src/tests/account.spec.js
+++ src/tests/account.spec.js
@@ -1,24 +1,54 @@
-const { test, expect } = require('@playwright/test');
-
+const { expect } = require('@playwright/test');
 const BasePage = require('../pages/base.page');
 const AuthenticationPage = require('../pages/authentication.page');
 const NavigationContainerPage = require('../pages/navigation-container.page');
+const CreateAccountPage = require('../pages/create-account.page');
+const CustomerAccountPage = require('../pages/customer-account.page');
 
-test.describe('Account Tests', () => {
-    test.beforeEach(async ({ page }) => {
+describe('Account Tests', () => {
+    beforeEach(async () => {
+        context = await browser.newContext();
+        page = await context.newPage();
+
         authenticationPage = new AuthenticationPage(page);
         basePage = new BasePage(page);
         navigationContainerPage = new NavigationContainerPage(page);
+        createAccountPage = new CreateAccountPage(page);
+        customerAccountPage = new CustomerAccountPage(page);
         await basePage.navigate('http://automationpractice.com/index.php');
         await navigationContainerPage.clickSignIn();
     });
 
-    test.afterEach(async ({ page }) => {
-        await page.close();
+    afterEach(async () => {
+        // await context.close();
     });
 
-    test('Should be able to create an account', async () => {
-        await authenticationPage.enterCreateEmail('test-bitpanda@gmail.com');
+    it.only('Should be able to create an account', async () => {
+        await authenticationPage.enterCreateEmail('test-panda-15@gmail.com');
         await authenticationPage.submitCreateEmail();
+        // await createAccountPage.selectGender('F');
+        // await createAccountPage.enterFirstname('firstaname');
+        // await createAccountPage.enterLastname('lastname');
+        // await createAccountPage.enterPassword('test_password');
+        // await createAccountPage.selectDayOfBirth('10');
+        // await createAccountPage.selectMonthOfBirth('5');
+        // await createAccountPage.selectYearOfBirth('1990');
+        // await createAccountPage.checkSignUpForNews();
+        // await createAccountPage.enterAddressFirstname('addressFirstname');
+        // await createAccountPage.enterAddressLastname('addressLastname');
+        // await createAccountPage.enterAddressCompany('test_company');
+        // await createAccountPage.enterAddrAddress1('addressAddress1');
+        // await createAccountPage.enterAddrCity('Huntsville');
+        // await page.locator('select[name="id_state"]').click();
+        const selector = await page.$('#id_state');
+        await selector?.selectOption({label: 'Arizona'});
+        // await createAccountPage.selectAddrState('Alabama');
+        // await createAccountPage.enterAddrPostcode('35004');
+        // await createAccountPage.selectAddrCountry('United States');
+        // await createAccountPage.enterAddrMobilePhone('202-555-0177');
+        await createAccountPage.clickSubmitAccount();
+        await expect(page.locator(customerAccountPage.info)).toContainText(
+            'Welcome to your account. Here you can manage all of your personal information and orders.',
+        );
     });
 });
