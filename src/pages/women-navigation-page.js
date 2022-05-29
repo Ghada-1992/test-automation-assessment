92775e505fd1608403fd09222c96b0a94bb39bf9
Ghada Ayari <gayari@iobeya.com>
2022-05-30 00:24:15 +0100

    update women navigation page

--- src/pages/women-navigation-page.js
+++ src/pages/women-navigation-page.js
@@ -5,15 +5,19 @@ class WomenNavigationPage extends BasePage {
         super(page);
 
         this.addToCartButton = '(//a[@title="Add to cart"])';
-        this.productImage = '(//div[@class="product-image-container"])'
+        this.productImage = '(//div[@class="product-image-container"])';
     }
 
     async clickAddToCart(index) {
         await this.page.click(this.addToCartButton + '[' + index + ']');
     }
-    
+
     async clickAddToCart(index) {
         await this.page.click(this.addToCartButton + '[' + index + ']');
+    }
+
+    async hoverProductImage(index) {
+        await this.page.hover(this.productImage + '[' + index + ']');
     }
 }
 module.exports = WomenNavigationPage;
